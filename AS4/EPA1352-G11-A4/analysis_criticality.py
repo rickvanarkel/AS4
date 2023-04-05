3d45a9a3e05c4d9f2dc5efcab4c34deb7fda864e
superjager1 <daan@jagernet.nl>
2023-04-05 14:03:45 +0200

    .

--- AS4/EPA1352-G11-A4/analysis_criticality.py
+++ AS4/EPA1352-G11-A4/analysis_criticality.py
@@ -5,31 +5,44 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import route_network as rn
 
 '''
 Er kunnen zowel analyses gedaan worden op de NetworkX als op de dataframe
 Voor nu zit er alleen een basisbestand in voor de analyse per road, en per road segment
 '''
 
-analysis_link = './data/traffic_network_compact_LB.csv'
+analysis_link = './data/traffic_network_LB.csv'
 analysis_df = pd.read_csv(analysis_link)
 
 criticality_columns = ['road', 'road segment', 'lrp', 'Truck number', 'Truck percentage']
-criticality_columns2 = ['road segment', 'Truck number', 'Truck percentage'] # Nu per road segment gekeken, kan op dezelfde manier met road
+criticality_columns2 = ['road', 'lrp', 'road segment', 'Truck number', 'Heavy Truck', 'Medium Truck', 'Small Truck', 'Truck percentage'] # Nu per road segment gekeken, kan op dezelfde manier met road
 
 criticality_df = analysis_df.loc[:, criticality_columns2]
 
 criticality_df_grouped = criticality_df.groupby('road segment').mean()
 criticality_df_grouped = criticality_df_grouped.reset_index()
 #criticality_df_grouped = criticality_df_grouped.sort_values('Truck number', ascending=False)
+total_df_grouped = criticality_df_grouped.copy()
 
-print(criticality_df_grouped)
-print(criticality_df_grouped.nlargest(10, 'Truck percentage'))
-print(criticality_df_grouped.nlargest(10, 'Truck number'))
 
-sns.histplot(criticality_df_grouped, x='Truck number')
-sns.histplot(criticality_df_grouped, x='Truck percentage')
-plt.show()
+
+analyze_columns_L = ['road segment', 'Heavy Truck', 'Medium Truck', 'Small Truck']
+criticality_df_heavy = criticality_df_grouped.loc[:, analyze_columns_L]
+
+for_loop = ['Heavy Truck', 'Medium Truck', 'Small Truck']
+for i in for_loop:
+    print(f'This DF shows the most critical segments sorted on the number of {i}: \n {criticality_df_heavy.nlargest(10, i)} \n')
+
+road_dict = rn.make_points_edges(analysis_df, id_l='id')
+G = rn.make_networkx(road_dict, analysis_df)
+rn.create_colored_network(G)
+
+total_df_grouped['weighted_total'] = total_df_grouped['Heavy Truck'] + 2*total_df_grouped['Medium Truck'] + 3*total_df_grouped['Small Truck']
+print(f'This DF shows the most critical segments sorted on the weighted total of trucks: \n {total_df_grouped.nlargest(10, "weighted_total")} \n')
+
+
+
 
 
 '''
--- AS4/EPA1352-G11-A4/route_network.py
+++ AS4/EPA1352-G11-A4/route_network.py
@@ -13,7 +13,7 @@ This function creates a dictionary which contains all roads as a key and a list 
 - temp_edges = Contains all edges in the right order
 - point_id = Contains all the node ID's in the right order
 """
-def make_points_edges(df):
+def make_points_edges(df, weight_label = 'length', id_l = "id"):
     road_dict = {}
     roads = df['road'].unique()
 
@@ -26,12 +26,14 @@ def make_points_edges(df):
         temp_id = 0
 
         for index, row in temp_df.iterrows():
-            temp_points.append((row['lon'], row['lat']))
-            point_ids.append(row['id'])
+            temp_points.append((row['lat'], row['lon']))
+            point_ids.append(row[id_l])
             if index != 0:
-                temp_edges.append((temp_id, row['id'], row['length']))
-            temp_id = row['id']
+                if row[id_l] != temp_id:
+                    temp_edges.append((temp_id, row[id_l], row[weight_label]))
+            temp_id = row[id_l]
         road_dict[road_type] = (temp_points, temp_edges, point_ids)
+
     return road_dict
 
 """
@@ -39,15 +41,18 @@ This function uses the road dict to add all points as nodes and then sets edges 
 """
 def make_networkx(road_dict, df):
     G = nx.Graph()
-    for roads in df['road'].unique():
+    roads_temp = df['road'].unique()
+    for roads in roads_temp:
         temp_list = road_dict[roads]
         points = temp_list[0]
         edges = temp_list[1]
         point_ids = temp_list[2]
+
         for i in range(len(point_ids)):
             G.add_node(point_ids[i], pos=points[i])
         for i in range(len(edges)):
             G.add_edge(edges[i][0], edges[i][1], weight = edges[i][2])
+
     return G
 
 """
@@ -55,6 +60,7 @@ This function is used to create the visual of the networkx graph
 """
 def create_graph(G):
     pos = nx.get_node_attributes(G, 'pos')
+
     nx.draw_networkx(G, pos=pos, node_color='k', with_labels=False)
     nx.draw_networkx(G, pos=pos, node_size=10, with_labels=False) # draw nodes and edges
     #nx.draw_networkx_labels(G, pos=pos)  # draw node labels/names
@@ -69,6 +75,19 @@ def create_graph(G):
     plt.axis()
     plt.show()
 
+def create_colored_network(G):
+    pos = nx.get_node_attributes(G, 'pos')
+
+    print("pos is klaar")
+    weights = [G[u][v]['weight'] for u,v in G.edges()]
+    normalized_weights = [(w - min(weights)) / (max(weights) - min(weights)) for w in weights]
+    colormap = plt.cm.RdYlGn_r
+    edge_colors = [colormap(x) for x in normalized_weights]
+
+    nx.draw_networkx(G, pos=pos, edge_color=edge_colors, node_color= "none", with_labels=False)
+
+    plt.axis()
+    plt.show()
 
 
 
